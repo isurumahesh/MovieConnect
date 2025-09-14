0cb10b976892666abc3c53c18fab2c3d61759787
isuruw <i.wickramasinghe@saltosystems.com>
2025-09-14 12:32:56 +0200

    squashed 3

--- MovieConnect.Core/Models/TestMain.cs
+++ MovieConnect.Core/Models/TestMain.cs
@@ -3,4 +3,6 @@ namespace MovieConnect.Core.Models;
 public class TestMain
 {
     public string NameTest { get; set; }
+    public string NameTestA { get; set; }
+    public string NameTestCCC { get; set; }
 }
